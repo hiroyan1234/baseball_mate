50db5dd935cb3d38dac3a36c66078d07fb3c8f17
EC2 Default User <ec2-user@ip-172-31-13-114.ap-northeast-1.compute.internal>
2023-03-07 12:39:48 +0000

    アドミンレイアウト修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,14 +1,14 @@
 class Admin::UsersController < ApplicationController
   before_action :admin_signed_in
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def index
-    @users = User.all
+    @users = User.page(params[:page])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -18,21 +18,21 @@ class Admin::UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   private
-  
+
   def admin_signed_in
     unless admin_signed_in?
       redirect_to new_admin_session_path
     end
   end
-  
+
   def user_params
     params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :telephone_number, :sex, :is_deleted, :age, :nickname, :introduction, :image, :password, :password_confirmation, :is_deleted)
   end
-  
+
 end
