b748cd2de5216895812e2a8bc5add79233246029
EC2 Default User <ec2-user@ip-172-31-13-114.ap-northeast-1.compute.internal>
2023-04-11 12:38:48 +0000

    バリデーション機能追加完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,8 @@ class User < ApplicationRecord
 
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
   validates :sex, presence: true
   validates :age, presence: true
   validates :telephone_number, format: { with: /\A\d{10,11}\z/ }, presence: true
